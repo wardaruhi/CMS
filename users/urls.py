18ff65508741d172cd08c1487f7023c7188d6439
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-03-01 18:28:45 -0600

    Commit2

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 #from . import views
-from .views import SignUpView
+from .views import SignUpView, ChangePasswordResetDoneSuccessView, ChangePasswordResetDoneView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('change_password/', ChangePasswordResetDoneView.as_view(), name='change_password'),
+    path('change_password_done', ChangePasswordResetDoneSuccessView.as_view(), name = 'change_password_done'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import views as auth_views, forms as auth_forms
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,12 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class ChangePasswordResetDoneView(auth_views.PasswordChangeView):
+    form_class = auth_forms.PasswordChangeForm
+    template_name = 'change_password.html'
+    success_url = reverse_lazy('change_password_done')
+
+class ChangePasswordResetDoneSuccessView(auth_views.PasswordChangeView):
+    form_class = auth_forms.PasswordChangeForm
+    template_name = 'change_password_done.html'
